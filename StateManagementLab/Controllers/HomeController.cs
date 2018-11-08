82f7736bfc4fdf44451346fbd46e34f6f1926462
Your Name <you@example.com>
2018-11-08 08:46:08 -0500

    state manage

---
+++ StateManagementLab/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StateManagementLab.Models;
+
+
+namespace StateManagementLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Register";
+            return View();
+        }
+        public ActionResult Result(User r)
+        {
+            if (r.UserName != null)
+            {
+                Session["UserName"] = r.UserName;
+                Session["Email"] = r.Email;
+                Session["Password"] = r.Password;
+                Session["Age"] = r.Age;
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            
+            ViewBag.name = Session["UserName"];
+            Session["UserName"] = null;
+            return View();
+        }
+        public ActionResult UserInfo(User r)
+        {
+            if (r.UserName != null)
+            {
+                Session["UserName"] = r.UserName;
+                Session["Email"] = r.Email;
+                Session["Passwrod"] = r.Password;
+                Session["Age"] = r.Age;
+
+            }
+        
+            return View();
+        }
+
+
+    }
+}
---
+++ StateManagementLab/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StateManagementLab.MvcApplication" Language="C#" %>
